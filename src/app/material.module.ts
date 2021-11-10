c4f106194867c6631a6cfda9422ddc91d55a9814
jmoragarios <jmoragarios@gmail.com>
2021-11-09 23:02:06 -0600

    Added json-server.js and angular material
    Also added shared/components/header component

---
+++ src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+
+@NgModule({
+    exports: [ 
+        MatToolbarModule,
+        MatIconModule
+    ]
+})
+
+export class MaterailModule {}
