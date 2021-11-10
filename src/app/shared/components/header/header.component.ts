e8a5990ab46587839d147b02050f40e05e0bdb57
jmoragarios <jmoragarios@gmail.com>
2021-11-10 09:55:41 -0600

    Added shared/services/shopping-cart service

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from '../../services/shopping-cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  quantity$ = this.shoppingCartSvc.quantityAction$;
+  total$ = this.shoppingCartSvc.totalAction$;
+  cart$ = this.shoppingCartSvc.cartAction$;
+  
+  constructor(
+    private shoppingCartSvc: ShoppingCartService
+  ) { }
 
-  ngOnInit(): void {
-  }
 
 }
